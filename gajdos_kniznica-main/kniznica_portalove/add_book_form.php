6333966329e4f2b9a46509a32e3589219db333b4
pali33311 <104359821+pali33311@users.noreply.github.com>
2022-12-07 01:53:58 +0100

    Update add_book_form.php

--- gajdos_kniznica-main/kniznica_portalove/add_book_form.php
+++ gajdos_kniznica-main/kniznica_portalove/add_book_form.php
@@ -12,6 +12,5 @@ $db = $GLOBALS['db'];
     <input type="text" name="rating"/><br>
     About:<br>
     <textarea name="about" style="width: 600px; height: 300px;"></textarea><br>
-    <input type="hidden" name="id" value="<?php echo $bookDetails[0]['id']; ?>">
-    <input type="submit" name="submit" value="Update">
+    <input type="submit" name="submit" value="Add">
 </form>
